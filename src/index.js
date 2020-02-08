3b1a403e3f9243139b9ddc6947f7b2b7e140bf9e
alvarooncode <alvarojsaavedra@gmail.com>
2020-02-08 15:44:07 +0100

    Credits

---
+++ src/index.js
@@ -0,0 +1,4 @@
+/**
+ * Original code by Moses Esan
+ * Source: https://medium.com/swlh/how-to-build-a-node-js-authentication-api-with-email-verification-image-upload-and-password-reset-95e35fd46be1
+ */
